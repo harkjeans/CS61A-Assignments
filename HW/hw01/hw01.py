ce7417b74e77566b5c692040ff5e6a3954299d8f
harkjeans <chengqi172@gmail.com>
2025-10-16 12:18:40 +0800

    lab00

--- HW/hw01/hw01.py
+++ HW/hw01/hw01.py
@@ -13,9 +13,9 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = sub
     else:
-        f = _____
+        f = add
     return f(a, b)
 
 def a_plus_abs_b_syntax_check():
@@ -42,7 +42,7 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    return i*i + j*j + k*k - max(i, j, k)**2
 
 def two_of_three_syntax_check():
     """Check that your two_of_three code consists of nothing but a return statement.
@@ -66,6 +66,8 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    
+    
 
 
 def hailstone(n):
