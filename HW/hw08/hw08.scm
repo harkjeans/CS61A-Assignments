1742ea07ea8c3a8d1a4ab8bd7e5b5a60ec4ece8d
harkjeans <chengqi172@gmail.com>
2025-11-03 13:44:13 +0800

    hw08

--- HW/hw08/hw08.scm
+++ HW/hw08/hw08.scm
@@ -1,7 +1,31 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s)
+    (cond ((null? s) #t)
+          ((null? (cdr s)) #t)
+          ((<= (car s) (car (cdr s)))
+           (ascending? (cdr s)))
+           (else #f)))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s)
+    (cond ((null? s) '())
+          ((pred (car s))
+           (cons (car s)
+                 (my-filter pred (cdr s))))
+          (else
+           (my-filter pred (cdr s)))))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+    (cond ((null? lst1) lst2)
+          ((null? lst2) lst1)
+          (else (cons (car lst1)
+                     (interleave lst2
+                                 (cdr lst1))))))
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s)
+    (if (null? s)
+        '()
+        (let ((first-element (car s))
+              (rest-element (cdr s)))
+            (cons first-element
+                  (no-repeats
+                   (filter (lambda (x) (not (= x first-element)))
+                         rest-element))))))
